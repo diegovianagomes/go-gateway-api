8a89c5aaaf39510978239c8ce4d4313ea5fe478c
dvgba <diegovianagomes01@gmail.com>
2025-04-09 11:56:42 -0300

    feat(domain): add common error definitions for domain layer
    This commit introduces a new file `errors.go` in the `domain` package, defining common error types such as `ErrAccountNotFound`, `ErrDuplicatedAPIKey`, `ErrInvoiceNotFound`, and `ErrUnauthorizedAccess`. These errors standardize error handling across the domain layer, improving consistency and maintainability.

---
+++ internal/domain/errors.go
@@ -0,0 +1,14 @@
+package domain
+
+import "errors"
+// ErrAccountNotFound is returned when an account is not found
+// ErrDuplcatedAPIKey is returned when an attempt is made to create an account with a duplicate API key
+// ErrInvoiceNotFound is returned when an invoice is not found
+// ErrUnauthorizedAcces is returned when an attempt is made to access a resource that the user does not have permission to access
+var (
+	
+	ErrAccountNotFound = errors.New("account not found")
+	ErrDuplicatedAPIKey = errors.New("api key already exists")
+	ErrInvoiceNotFound = errors.New("invoice not found")
+	ErrUnauthorizedAcces = errors.New("unauthorized acces")
+)
