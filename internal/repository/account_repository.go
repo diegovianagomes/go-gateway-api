d2915d0c1c39ff10c1aaa6a71c25c2b03846f850
dvgba <diegovianagomes01@gmail.com>
2025-04-09 19:13:07 -0300

    refactor(repository): fix spacing and add unimplemented FindByID method
    The commit addresses minor code style issues by fixing spacing inconsistencies in the UpdateBalance method. Additionally, it adds an unimplemented FindByID method to the AccountRepository struct, ensuring the repository interface is complete.

--- internal/repository/account_repository.go
+++ internal/repository/account_repository.go
@@ -12,6 +12,11 @@ type AccountRepository struct {
 	db *sql.DB
 }
 
+// FindByID implements domain.AccountRepository.
+func (r *AccountRepository) FindByID(id string) (*domain.Account, error) {
+	panic("unimplemented")
+}
+
 // constructor
 func NewAccountRepository(db *sql.DB) *AccountRepository {
 	return &AccountRepository{db: db}
@@ -102,7 +107,7 @@ func (r *AccountRepository) FindById(id string) (*domain.Account, error) {
 // Method to update account balance
 func (r *AccountRepository) UpdateBalance(account *domain.Account) error {
 	tx, err := r.db.Begin()
-	if err!= nil {
+	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
@@ -111,20 +116,20 @@ func (r *AccountRepository) UpdateBalance(account *domain.Account) error {
 	// The FOR UPDATE clause locks the selected rows until the end of the transaction
 	var currentBalance float64
 	err = tx.QueryRow(`
-		SELECT balance FROM accounts WHERE id = $1 FOR UPDATE`,  account.ID).Scan(&currentBalance)
+		SELECT balance FROM accounts WHERE id = $1 FOR UPDATE`, account.ID).Scan(&currentBalance)
 	// Error no rows means account not found or another error
 	if err == sql.ErrNoRows {
 		return domain.ErrAccountNotFound
 	}
-	if err!= nil {
+	if err != nil {
 		return err
 	}
 
 	// Update the account balance
 	_, err = tx.Exec(`
 		UPDATE accounts SET balance = $1, updated_at = $2 WHERE id = $3
-	`, currentBalance + account.Balance, time.Now(), account.ID)
-	if err!= nil {
+	`, currentBalance+account.Balance, time.Now(), account.ID)
+	if err != nil {
 		return err
 	}
 	return tx.Commit()
