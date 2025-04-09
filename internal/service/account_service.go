2ed75dab80f19fb9471a8b69a008980dbd698d52
dvgba <diegovianagomes01@gmail.com>
2025-04-09 17:56:27 -0300

    feat(account_service): add methods for account balance update and retrieval
    This commit introduces new methods to the AccountService: UpdateBalance, FindByAPIKey, and FindByID. These methods enable updating an account's balance in a thread-safe manner and retrieving account details by API key or ID. Additionally, a comment was added to the CreateAccount method to clarify its behavior regarding duplicate API key validation.

--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -14,6 +14,8 @@ func NewAccountService(repository domain.AccountRepository) *AccountService {
 	return &AccountService{repository: repository}
 }
 
+// CreateAccount creates a new account and validates duplicate API Key
+// Returns ErrDuplicatedAPIKey if the key already exists
 func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
 	account := dto.ToAccount(input)
 
@@ -34,5 +36,44 @@ func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.Accou
 
 	output := dto.FromAccount(account)
 	return output, nil
+}
 
+// UpdateBalance updates the balance of an account in a thread-safe way
+// The amount can be positive (credit)
+func (s *AccountService) UpdateBalance(apiKey string, amount float64) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AddBalance(amount)
+	err = s.repository.UpdateBalance(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return output, nil
+}
+
+// FindByAPIKey searches for an account by API Key
+func (s *AccountService) FindByAPIKey(apiKey string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return output, nil
+
+}
+
+// FindByID searches for an account by ID
+func (s *AccountService) FindByID(id string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+	output := dto.FromAccount(account)
+	return output, nil
 }
