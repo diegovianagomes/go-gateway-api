ea332b6f46e5813c3dcb64814860912cf474d81f
dvgba <diegovianagomes01@gmail.com>
2025-04-10 19:06:23 -0300

    feat(invoice): add invoice service with create, get, and list operations
    Introduce a new InvoiceService to handle invoice-related operations, including creating invoices, fetching invoices by ID, and listing invoices by account or API key. The service integrates with the account service and repository to ensure proper authorization and data persistence. Additionally, fix a typo in the `ErrUnauthorizedAccess` error variable name in the errors.go file.

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -1,6 +1,7 @@
 package domain
 
 import "errors"
+
 // ErrAccountNotFound is returned when an account is not found
 // ErrDuplcatedAPIKey is returned when an attempt is made to create an account with a duplicate API key
 // ErrInvoiceNotFound is returned when an invoice is not found
@@ -8,11 +9,10 @@ import "errors"
 // ErrInvalidAmount is returned when an attempt is made to create an invoice with an invalid amount
 // ErrInvalidStatus is returned when an attempt is made to create an invoice with an invalid status
 var (
-	
-	ErrAccountNotFound = errors.New("account not found")
-	ErrDuplicatedAPIKey = errors.New("api key already exists")
-	ErrInvoiceNotFound = errors.New("invoice not found")
-	ErrUnauthorizedAcces = errors.New("unauthorized acces")
-	ErrInvalidAmount = errors.New("invalid amount")
-	ErrInvalidStatus = errors.New("invalid status")
+	ErrAccountNotFound    = errors.New("account not found")
+	ErrDuplicatedAPIKey   = errors.New("api key already exists")
+	ErrInvoiceNotFound    = errors.New("invoice not found")
+	ErrUnauthorizedAccess = errors.New("unauthorized access")
+	ErrInvalidAmount      = errors.New("invalid amount")
+	ErrInvalidStatus      = errors.New("invalid status")
 )
